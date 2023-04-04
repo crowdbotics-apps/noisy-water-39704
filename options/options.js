86cc655aec6af2702b18758271f7a6c5aeaf73d0
Cary McLaughlin <invoices.cary@outlook.com>
2023-04-04 10:54:15 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "noisy-water-39704",
   url: "https://noisy-water-39704.botics.co",
   api: "https://noisy-water-39704.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
